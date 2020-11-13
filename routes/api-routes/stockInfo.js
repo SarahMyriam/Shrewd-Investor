2a32c318986f6a0867faf0d18d6e6bc02480b07b
Sam <SApuzzo527@gmail.com>
2020-11-12 21:54:24 -0500

    added stockInfoJs Talia file

---
+++ routes/api-routes/stockInfo.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const router = express.Router();
+require('dotenv').config();
+console.log(process.env.API_KEY);
+const axios = require('axios');
+//get stock info for IBM
+router.get("/IBM", (req, res, next) => {
+    // console.log("I am here");
+    const IBM = "IBM"
+    axios
+    .get(`https://cloud.iexapis.com/stable/stock/${IBM}/quote?token=${process.env.API_KEY}`
+    ).then( ({ data }) => {
+        //   console.log(data);
+        //   res.redirect("/");
+    //    optionally return data created
+        console.log("IBM");
+        res.json(data);
+       console.log(data.companyName);
+       console.log(data.iexRealtimePrice);
+       console.log(data.change);
+       console.log(data.changePercent);
+       })
+       .catch(err => {
+            console.log(err);
+          res.status(500);
+          next(err);
+       });       
+ });
+ //get stock info for tesla
+ router.get("/TSLA", (req, res, next) => {
+    console.log("I am here");
+    const TSLA = "TSLA"
+    axios
+    .get(`https://cloud.iexapis.com/stable/stock/${TSLA}/quote?token=${process.env.API_KEY}`
+    ).then( ({ data }) => {
+        //   console.log(data);
+        //   res.redirect("/");
+    //    optionally return data created
+        console.log("Tesla");
+        res.json(data);
+       console.log(data.companyName);
+       console.log(data.iexRealtimePrice);
+       console.log(data.change);
+       console.log(data.changePercent);
+       })
+       .catch(err => {
+            console.log(err);
+          res.status(500);
+          next(err);
+       });
+ });
+ //get stock infor for biogen
+ router.get("/BIIB", (req, res, next) => {
+    console.log("I am here");
+    const BIIB = "BIIB"
+    axios
+    .get(`https://cloud.iexapis.com/stable/stock/${BIIB}/quote?token=${process.env.API_KEY}`
+    ).then( ({ data }) => {
+        //   console.log(data);
+        //   res.redirect("/");
+    //    optionally return data created
+        console.log("Biogen");
+        res.json(data);
+       console.log(data.companyName);
+       console.log(data.iexRealtimePrice);
+       console.log(data.change);
+       console.log(data.changePercent);
+       })
+       .catch(err => {
+            console.log(err);
+          res.status(500);
+          next(err);
+       });
+ });
+ module.exports = router;
